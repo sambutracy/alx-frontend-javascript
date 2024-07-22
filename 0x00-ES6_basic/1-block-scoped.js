4eac42520890973e9bdc4271013794f320506211
sambutracy <tracysambut@gmail.com>
2024-07-22 21:13:04 +0300

    fix comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;  // Use let for block scope
-    let task2 = true;  // Use let for block scope
+    let task = false; 
+    let task2 = true;
   
     if (trueOrFalse) {
-      let task = false;  // New block-scoped variable
-      let task2 = true;  // New block-scoped variable
+      let task = false;
+      let task2 = true;
     }
   
     return [task, task2];

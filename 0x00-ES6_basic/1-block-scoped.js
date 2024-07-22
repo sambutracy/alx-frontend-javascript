cb37f2f6a9ced4acaef6a353e4e72ef53dddada5
sambutracy <tracysambut@gmail.com>
2024-07-22 22:55:52 +0300

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; 
-  let task2 = true;
-  
+  const task = false; // Use const for variables that won't be reassigned
+  const task2 = true; // Use const for variables that won't be reassigned
+
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    const task = true; // Block-scoped variable
+    const task2 = false; // Block-scoped variable
   }
-  
+
   return [task, task2];
-}  
+}
+ 

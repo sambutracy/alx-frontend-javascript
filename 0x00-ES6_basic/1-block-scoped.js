e33004607d6e7ac530737b58dc6cfd8ab61b5d29
sambutracy <tracysambut@gmail.com>
2024-07-22 21:18:04 +0300

    fix task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
   
   if (trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    task = false;
+    task2 = true;
   }
   
   return [task, task2];

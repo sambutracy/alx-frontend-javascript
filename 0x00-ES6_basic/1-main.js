55661a79b2404f071a029d49fbffa669303e4f21
sambutracy <tracysambut@gmail.com>
2024-07-23 00:25:38 +0300

    fix eslintrc

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,4 +1,4 @@
-import taskBlock from './1-block-scoped.js';
+import taskBlock from './1-block-scoped';
 
-console.log(taskBlock(true));  // Should log: [ true, false ]
+console.log(taskBlock(true)); // Should log: [ true, false ]
 console.log(taskBlock(false)); // Should log: [ false, true ]

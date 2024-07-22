016e699bbb3b2055b5711fad8e97919d622a4874
sambutracy <tracysambut@gmail.com>
2024-07-22 21:15:58 +0300

    fix indent

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false; 
+  let task = false; 
+  let task2 = true;
+  
+  if (trueOrFalse) {
+    let task = false;
     let task2 = true;
+  }
   
-    if (trueOrFalse) {
-      let task = false;
-      let task2 = true;
-    }
-  
-    return [task, task2];
-  }  
+  return [task, task2];
+}  

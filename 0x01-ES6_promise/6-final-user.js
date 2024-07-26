ac7b04122a42d0992d692884973f87dbb48c22ad
sambutracy <tracysambut@gmail.com>
2024-07-27 01:48:58 +0300

    fix eroor task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,19 +4,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the functions and get their promises
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  // Use Promise.allSettled to handle both promises
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then(results => {
-      // Format the results into the required structure
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
 
 export default handleProfileSignup;

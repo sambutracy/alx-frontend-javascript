d12305c081754a6e5bf8db587eb8073ca1dbb2b3
sambutracy <tracysambut@gmail.com>
2024-07-27 00:58:21 +0300

    fix errors task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
 // 0-promise.js
 
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Example logic to resolve the promise
-      resolve("API response");
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    // Example logic to resolve the promise
+    resolve('API response');
+  });
+}
+
+export default getResponseFromAPI;

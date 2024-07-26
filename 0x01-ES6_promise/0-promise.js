638d4980b2e53d7d7c3a4b3aae77e49ec4dd75fc
sambutracy <tracysambut@gmail.com>
2024-07-27 00:21:00 +0300

    Add task 0 and other files

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+// 0-promise.js
+
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Example logic to resolve the promise
+      resolve("API response");
+    });
+  }
+  
+  export default getResponseFromAPI;
+  

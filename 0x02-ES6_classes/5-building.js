cd2ca1c57e3355b898b8837d1f54e006381b67e2
sambutracy <tracysambut@gmail.com>
2024-07-26 04:00:56 +0300

    add task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+// 5-building.js
+
+class Building {
+    constructor(sqft) {
+      this._sqft = this._validateSqft(sqft);
+      if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  
+    // Validate sqft
+    _validateSqft(sqft) {
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      return sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  }
+  
+  export default Building;
+  

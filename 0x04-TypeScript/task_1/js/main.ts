c3eeec361f8bb978c661d084053aee5301bd8140
sambutracy <tracysambut@gmail.com>
2024-08-09 16:12:35 +0300

    add

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Define the Teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // Index signature to allow additional properties
+}
+
+// Create a teacher object
+const teacher3: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+
+// Print the teacher object
+console.log(teacher3);
